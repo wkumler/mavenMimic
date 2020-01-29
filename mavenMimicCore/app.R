7e1a89bf9c399c08e2708adc749c86036fbe6007
wkumler <wkumler@uw.edu>
2020-01-29 11:31:34 -0800

    Updated Shiny metadata and prepared skeleton

--- mavenMimicCore/app.R
+++ mavenMimicCore/app.R
@@ -1,18 +1,60 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Setup things ----
 
 library(shiny)
 library(plotly)
+library(dplyr)
 
-# Load the raw mass data
+# Functions ----
 
-# Define UI for application that draws a histogram
+
+pmppm <- function(mass, ppm=4){c(mass*(1-ppm/1000000), mass*(1+ppm/1000000))}
+
+plotGivenEIC <- function(mass, ppm=5, df=raw_data_frame, plotTIC=FALSE){
+    eic <- df %>% 
+        filter(mz>min(pmppm(mass, ppm = ppm))&mz<max(pmppm(mass, ppm = ppm))) %>% 
+        group_by(file, rt) %>% 
+        summarize(EIC_int=sum(int)) %>%
+        mutate(sample_group=c("DCM", "25m")[ceiling(file/3)%%2+1]) %>%
+        mutate(spindir=c("Cyclone", "Anticyclone")[(1-ceiling(file/12)%%2)+1])
+    if(plotTIC){
+        TIC_df <- df %>% 
+            mutate(rt=round(rt)) %>% 
+            group_by(rt) %>% 
+            summarize(TIC=sum(int)) %>%
+            mutate(TIC=(TIC/max(TIC))*max(eic$EIC_int))
+        plot_ly() %>%
+            add_trace(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, opacity = 0.5,
+                      mode="lines", type="scatter",
+                      colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
+            add_trace(data = TIC_df, x=~rt, y=~TIC, 
+                      mode="lines", type="scatter", line=list(color="black"),
+                      hoverinfo="none") %>%
+            layout(xaxis = list(title = "Retention time (s)"),
+                   yaxis = list(title = "Intensity",
+                                fixedrange = TRUE))
+    } else {
+        plot_ly(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, alpha = 0.5,
+                mode="lines", type="scatter",
+                colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
+            layout(xaxis = list(title = "Retention time (s)"),
+                   yaxis = list(title = "Intensity",
+                                fixedrange = TRUE))
+    }
+}
+
+plotGivenScan <- function(ret, window=1, df=raw_data_frame){
+    scandata <- df %>% 
+        filter(rt>ret-window/2&rt<ret+window/2) %>% 
+        mutate(mz=round(mz*100)/100) %>%
+        group_by(mz) %>% 
+        summarize(TIS=sum(int))
+    plot_ly(data=scandata, x=~mz, y=~TIS, type = "bar") %>%
+        layout(xaxis = list(title = "m/z"),
+               yaxis = list(title = "Intensity",
+                            fixedrange = TRUE))
+}
+
+# UI ----
 ui <- fluidPage(
 
     # Application title
@@ -21,30 +63,31 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            numericInput(inputId = "given_mz",
+                        "m/z:",
+                        value = 118.0868),
+            numericInput(inputId = "given_ppm",
+                         "+/- (ppm)",
+                         value = 5)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("chrom"),
+           plotOutput("TIS")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Server ----
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$chrom <- renderPlot({
+        plot(10:1)
+    })
+    
+    output$TIS <- renderPlot({
+        plot(1:10)
     })
 }
 
