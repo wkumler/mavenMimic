5718ce2e213377b258777bd167e4de16c4cfd14e
wkumler <wkumler@uw.edu>
2020-01-30 15:45:53 -0800

    Returned app to functional state, albeit using in-memory dataframe

--- app.R
+++ app.R
@@ -11,56 +11,52 @@ library(RSQLite)
 pmppm <- function(mass, ppm=4){c(mass*(1-ppm/1000000), mass*(1+ppm/1000000))}
 
 plotGivenEIC <- function(mass, ppm=5, df=raw_data_frame, plotTIC=FALSE){
-    eic <- df %>% 
-        filter(mz>min(pmppm(mass, ppm = ppm))&mz<max(pmppm(mass, ppm = ppm))) %>% 
-        group_by(fileid, rt) %>% 
-        summarize(EIC_int=sum(int)) %>%
-        mutate(sample_group=c("DCM", "25m")[ceiling(fileid/3)%%2+1]) %>%
-        mutate(spindir=c("Cyclone", "Anticyclone")[(1-ceiling(fileid/12)%%2)+1])
-    if(plotTIC){
-        TIC_df <- df %>% 
-            mutate(rt=round(rt)) %>% 
-            group_by(rt) %>% 
-            summarize(TIC=sum(int)) %>%
-            mutate(TIC=(TIC/max(TIC))*max(eic$EIC_int))
-        plot_ly(source = "EIC") %>%
-            add_trace(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, opacity = 0.5,
-                      mode="lines", type="scatter",
-                      colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
-            add_trace(data = TIC_df, x=~rt, y=~TIC, 
-                      mode="lines+markers", type="scatter", line=list(color="black"),
-                      hoverinfo="none") %>%
-            layout(xaxis = list(title = "Retention time (s)"),
-                   yaxis = list(title = "Intensity",
-                                fixedrange = TRUE))
-    } else {
-        plot_ly(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, alpha = 0.5,
-                mode="lines+markers", type="scatter",
-                colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone")),
-                source = "EIC") %>%
-            layout(xaxis = list(title = "Retention time (s)"),
-                   yaxis = list(title = "Intensity",
-                                fixedrange = TRUE))
-    }
+  eic <- df %>% 
+    filter(mz>min(pmppm(mass, ppm = ppm))&mz<max(pmppm(mass, ppm = ppm))) %>% 
+    group_by(file, rt) %>% 
+    summarize(EIC_int=sum(int)) %>%
+    mutate(sample_group=c("DCM", "25m")[ceiling(file/3)%%2+1]) %>%
+    mutate(spindir=c("Cyclone", "Anticyclone")[(1-ceiling(file/12)%%2)+1])
+  if(plotTIC){
+    TIC_df <- df %>% 
+      mutate(rt=round(rt)) %>% 
+      group_by(rt) %>% 
+      summarize(TIC=sum(int)) %>%
+      mutate(TIC=(TIC/max(TIC))*max(eic$EIC_int))
+    plot_ly(source = "EIC") %>%
+      add_trace(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, opacity = 0.5,
+                mode="lines", type="scatter",
+                colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
+      add_trace(data = TIC_df, x=~rt, y=~TIC, 
+                mode="lines", type="scatter", line=list(color="black"),
+                hoverinfo="none") %>%
+      layout(xaxis = list(title = "Retention time (s)"),
+             yaxis = list(title = "Intensity",
+                          fixedrange = TRUE))
+  } else {
+    plot_ly(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, alpha = 0.5,
+            mode="lines", type="scatter", source="EIC",
+            colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
+      layout(xaxis = list(title = "Retention time (s)"),
+             yaxis = list(title = "Intensity",
+                          fixedrange = TRUE))
+  }
 }
 
 plotGivenScan <- function(ret, window=1, df=raw_data_frame){
-    scandata <- df %>% 
-        filter(rt>ret-window/2&rt<ret+window/2) %>% 
-        mutate(mz=round(mz*1000)/1000) %>%
-        group_by(mz) %>% 
-        summarize(TIS=sum(int))
-    plot_ly(source = "TIS") %>%
-        add_trace(data=scandata, x=~mz, y=~TIS, 
-                  type = "bar", marker=list(color="black"),
-                  hoverinfo="none") %>%
-        add_trace(data=scandata, x=~mz, y=~TIS, 
-                  type="scatter", mode="markers", 
-                  marker=list(color="black")) %>%
-        layout(xaxis = list(title = "m/z"),
-               yaxis = list(title = "Intensity",
-                            fixedrange = TRUE)) %>%
-        event_register("plotly_click")
+  scandata <- df %>% 
+    filter(rt>ret-window/2&rt<ret+window/2) %>% 
+    mutate(mz=round(mz*100)/100) %>%
+    group_by(mz) %>% 
+    summarize(TIS=sum(int))
+  plot_ly(source = "TIS") %>%
+    add_trace(data=scandata, x=~mz, y=~TIS, type = "bar", 
+              marker=list(color="black"), hoverinfo="none") %>%
+    add_trace(data=scandata, x=~mz, y=~TIS, marker=list(color="black"),
+              type = "scatter", mode="markers") %>%
+    layout(xaxis = list(title = "m/z"),
+           yaxis = list(title = "Intensity",
+                        fixedrange = TRUE))
 }
 
 # UI ----
@@ -98,6 +94,7 @@ server <- function(input, output) {
     })
     
     output$debug <- renderPrint({
+      event_data(event = "plotly_click", source = "EIC")
     })
     
     output$TIS <- renderPlotly({
