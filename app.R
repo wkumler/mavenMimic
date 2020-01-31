0b4336bd21b77d22adbdfcccab5abec7ed6b25e3
wkumler <wkumler@uw.edu>
2020-01-30 16:53:55 -0800

    Fully functional app w TIC

--- app.R
+++ app.R
@@ -6,35 +6,33 @@ library(dplyr)
 library(RSQLite)
 
 #load("raw_data_frame")
+tic <- raw_data_frame %>% mutate(rt=round(rt)) %>%
+  group_by(rt) %>% summarize(int=sum(int))
 
 # Functions ----
 pmppm <- function(mass, ppm=4){c(mass*(1-ppm/1000000), mass*(1+ppm/1000000))}
 
-plotGivenEIC <- function(mass, ppm=5, df=raw_data_frame, plotTIC=FALSE){
+plotGivenEIC <- function(mass, ppm=5, df=raw_data_frame, plottic=TRUE){
   eic <- df %>% 
     filter(mz>min(pmppm(mass, ppm = ppm))&mz<max(pmppm(mass, ppm = ppm))) %>% 
     group_by(file, rt) %>% 
-    summarize(EIC_int=sum(int)) %>%
+    summarize(int=sum(int)) %>%
     mutate(sample_group=c("DCM", "25m")[ceiling(file/3)%%2+1]) %>%
     mutate(spindir=c("Cyclone", "Anticyclone")[(1-ceiling(file/12)%%2)+1])
-  if(plotTIC){
-    TIC_df <- df %>% 
-      mutate(rt=round(rt)) %>% 
-      group_by(rt) %>% 
-      summarize(TIC=sum(int)) %>%
-      mutate(TIC=(TIC/max(TIC))*max(eic$EIC_int))
+  if(plottic){
+    tic$int <- (tic$int/max(tic$int))*max(eic$int)
     plot_ly(source = "EIC") %>%
-      add_trace(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, opacity = 0.5,
+      add_trace(data = eic, x = ~rt, y = ~int, color = ~spindir, opacity = 0.5,
                 mode="lines", type="scatter",
                 colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
-      add_trace(data = TIC_df, x=~rt, y=~TIC, 
+      add_trace(data = tic, x=~rt, y=~int, 
                 mode="lines", type="scatter", line=list(color="black"),
                 hoverinfo="none") %>%
       layout(xaxis = list(title = "Retention time (s)"),
              yaxis = list(title = "Intensity",
                           fixedrange = TRUE))
   } else {
-    plot_ly(data = eic, x = ~rt, y = ~EIC_int, color = ~spindir, alpha = 0.5,
+    plot_ly(data = eic, x = ~rt, y = ~int, color = ~spindir, alpha = 0.5,
             mode="lines", type="scatter", source="EIC",
             colors = setNames(c("blue", "green"), c("Cyclone", "Anticyclone"))) %>%
       layout(xaxis = list(title = "Retention time (s)"),
@@ -69,13 +67,13 @@ ui <- fluidPage(
                         value = 118.0868),
             numericInput(inputId = "given_ppm",
                          "+/- (ppm)",
-                         value = 5)
+                         value = 50)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
            plotlyOutput("chrom"),
-           verbatimTextOutput("debug"),
+           #verbatimTextOutput("debug"),
            plotlyOutput("TIS")
         )
     )
@@ -85,7 +83,7 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$chrom <- renderPlotly({
-        TIS_data <- event_data(event = "plotly_click", source="TIS")
+        suppressWarnings(TIS_data <- event_data(event = "plotly_click", source="TIS"))
         if(is.null(TIS_data)){
             plotGivenEIC(input$given_mz, ppm=input$given_ppm)
         } else {
