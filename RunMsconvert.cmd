888168251d1aa413246fc6f4665ed15fea14a46b
wkumler <wkumler@uw.edu>
2020-02-05 17:41:45 -0800

    Copied the msconvert command into this directory for reproducibility

---
+++ RunMsconvert.cmd
@@ -0,0 +1,2 @@
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs/
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\MSMS\*DDApos*.raw --mzML --filter "peakPicking true 1-" -z -o mzMLs\MSMS\
