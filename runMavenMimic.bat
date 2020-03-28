7bc91df8bc35c27bbd54365011244deac150a123
wkumler <wkumler@uw.edu>
2020-03-27 22:58:23 -0700

    Created desktop shortcut and renamed browser window

--- runMavenMimic.bat
+++ runMavenMimic.bat
@@ -1,3 +1,3 @@
 start "" "C:\Program Files\R\R-3.6.2\bin\Rscript.exe" "G:\My Drive\mavenMimic\app.R"
 timeout /t 10
-start "mavenMimic" "http://127.0.0.1:1337/"
+start "Maven Mimic" "http://127.0.0.1:1337/"
