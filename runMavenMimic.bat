1d327c35a591081e3ae02947372bef6871ece9fd
wkumler <wkumler@uw.edu>
2020-05-15 12:24:22 -0700

    Updated to use latest version of R (4.0.0)

--- runMavenMimic.bat
+++ runMavenMimic.bat
@@ -1,3 +1,3 @@
-start "" "C:\Program Files\R\R-3.6.2\bin\Rscript.exe" "G:\My Drive\mavenMimic\app.R"
+start "" "C:\Program Files\R\R-4.0.0\bin\Rscript.exe" "G:\My Drive\mavenMimic\app.R"
 timeout /t 15
 start "Maven Mimic" "http://127.0.0.1:1337/"
