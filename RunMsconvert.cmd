1a04a75549390c7a94a6d87a661942b0462c4819
wkumler <wkumler@uw.edu>
2020-05-19 14:41:41 -0700

    Adding msconvert code for new blanks from Katherine

--- RunMsconvert.cmd
+++ RunMsconvert.cmd
@@ -1,2 +1,3 @@
 msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o falkor_mzMLs/
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2017\170706_Gradients2.0_CruiseFilters_2\170706_Blk_Blk0p2_?.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o falkor_mzMLs/
 msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\MSMS\*DDApos*.raw --mzML --filter "peakPicking true 1-" -z -o falkor_mzMLs\MSMS\
